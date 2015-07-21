2d1e5747d30205fc4dd352710d6fd588942cbcdc
Javis Sullivan <javissullivan@gmail.com>
2015-07-21 18:25:01 -0400

    Added more functionality using nltp requests and others

--- app.py
+++ app.py
@@ -1,24 +1,76 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from config import *
 from flask.ext.sqlalchemy import SQLAlchemy
+from stop_words import stop 
+from collections import Counter
+from bs4 import BeautifulSoup
+import operator
+import requests
+import re
+import nltk
+
+
+#################
+# Configuration #
+#################
 
 app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)
 db = SQLAlchemy(app)
 
-from models import Result
+''' will need to use models."Whatever class in models" for this to work'''
+#import models # fixed circular import
+from models import results
 
-#Print config vars
-#for set in app.config:
-#    print "{}: {}".format(set, app.config[set])
+#################
+#     routes    #
+################# 
 
-@app.route('/')
-def hello():
-    return "Hello World"
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    errors = []
+    results = {}
+    if request.method == 'POST':
+        # get url that the user has entered
+        try:  
+            url = request.form['url']
+            r = requests.get(url) # shooting out a request to get html eventually
+            results['raw_text'] = r.text
+        except:
+            errors.append("Unable to get URL. Please make sure it's valid and try again.")
+        if r:
+        	# text processing
+        	raw = BeautifulSoup(r.text).get_text()
+        	nltk.data.path.append('./nltk_data/')
+        	tokens = nltk.word_tockenize(raw)
+        	text = nltk.Text(tokens)
 
-@app.route('/<name>')
-def hello_name(name):
-    return "Hello {}!".format(name)
+        	# remove punctuation, count raw words
+        	nonPunct = re.compile('.*[A-Za-z].*')
+        	raw_words = [w for w in text if nonPunct.match(w)]
+        	raw_word_count = Counter(raw_words)
+
+        	# stop words
+        	no_stop_words = [w for w in raw_words if w.lower() not in stops]
+        	no_stop_words_count = Counter(no_stop_words)
+
+        	# save the results to database
+        	results = sorted(
+        			no_stop_words_count.items(),
+        			key=operator.itemgetter(1),
+        			reverse=True
+        		)
+        	try:
+        		result = Result(
+        			url=url,
+        			result_all=raw_word_count,
+        			result_no_stop_words=no_stop_words_count
+        			)
+        		db.session.add(result)
+        		db.session.commit()
+        	except:
+        		errors.append("Unable to add item todatabase.")
+    return render_template('index.html', errors=errors, results=results)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=8000)
