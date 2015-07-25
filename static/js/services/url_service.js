dc26224c21a5ebcb9ec1cee83a6f041fdda281c4
Javis Sullivan <javissullivan@gmail.com>
2015-07-24 20:07:45 -0400

    more

--- static/js/services/url_service.js
+++ static/js/services/url_service.js
@@ -13,15 +13,9 @@ app.factory('url_service', ['$http', function($http){
 		getUrlResults: function(userInput){
 			return $http.post('/start', {'url': userInput})
 				.success(function(data){
-					console.log('data received from service: ' + data);
-					for (prop in data){
-
-						console.log(prop + ': ' + data[prop]);
-					}
 					return data;
 				})
 				.error(function(err){
-					console.log('errors received from service: ' + err);
 					return err;
 				});
 		}
